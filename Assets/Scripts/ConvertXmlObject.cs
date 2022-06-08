16cba90043c7a564aacf8c1752f3de159b208aaf
DoritoTheChips <44832218+DoritoTheChips@users.noreply.github.com>
2022-06-08 22:46:35 +0200

    Fixed object scale and rotation (kinda)
    If you have a more stable rotation conversion feel free to commit one

--- Assets/Scripts/ConvertXmlObject.cs
+++ Assets/Scripts/ConvertXmlObject.cs
@@ -1,22 +1,25 @@
+using System.Security.Cryptography;
+using System.Xml;
 using UnityEngine;
 using UnityEditor;
-using System.Xml;
 
 public class ConvertXmlObject : MonoBehaviour
 {
     public string objectToConvert;
     int length;
+    float zRot;
     bool StopCounting;
     GameObject lastImage;
+    GameObject actualObject;
+    GameObject dummyObject;
 
-    [MenuItem("Vectorier/objects.xml")]
+    [MenuItem("Vectorier/Convert from objects.xml")]
     public static void ConvertXmlToObject()
     {
         Debug.Log("Converting...");
         //Open the object.xml 
         XmlDocument xml = new XmlDocument();
         xml.Load(Application.dataPath + "/XML/objects.xml");
-
         //Search for the selected object in the object.xml
         foreach (XmlNode node in xml.DocumentElement.SelectSingleNode("/Root/Objects"))
         {
@@ -41,14 +44,40 @@ public class ConvertXmlObject : MonoBehaviour
 
     void InstantiateImage(XmlNode images)
     {
-        //TODO : Fix the duplicate object
         //Place the image using every information the xml provide (X, Y, Width, Height, ClassName)
+        if (actualObject == null)
+        {
+            //Create a new GameObject with the selected object
+            actualObject = Instantiate(new GameObject(GameObject.FindObjectOfType<ConvertXmlObject>().objectToConvert), new Vector3(0, 0, 0), Quaternion.identity);
+            DestroyImmediate(GameObject.Find(GameObject.FindObjectOfType<ConvertXmlObject>().objectToConvert)); //Destroy duplicate
+            actualObject.name = GameObject.FindObjectOfType<ConvertXmlObject>().objectToConvert; //Name it correctly
+        }
 
         lastImage = Instantiate(
-        new GameObject(images.Attributes.GetNamedItem("ClassName").Value), //Usage of ClassName value (To name the new object)
-        new Vector3(float.Parse(images.Attributes.GetNamedItem("X").Value), float.Parse(images.Attributes.GetNamedItem("Y").Value), 0), //Usage of X and Y value
+        dummyObject = new GameObject(images.Attributes.GetNamedItem("ClassName").Value), //Usage of ClassName value (To name the new object)
+        new Vector3(float.Parse(images.Attributes.GetNamedItem("X").Value) / 100, float.Parse(images.Attributes.GetNamedItem("Y").Value) / 100, 0), //Usage of X and Y value (divided by 100 to fit Unity scale)
         Quaternion.identity);
-        lastImage.transform.localScale = new Vector3(float.Parse(images.Attributes.GetNamedItem("Width").Value), float.Parse(images.Attributes.GetNamedItem("Height").Value), 0); //Usage of Width and Height value
+
         lastImage.AddComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Textures/" + images.Attributes.GetNamedItem("ClassName").Value); //REUsage of ClassName value (To place the texture)
+        lastImage.GetComponent<SpriteRenderer>().transform.parent = actualObject.transform; //Place the new image into the selected object
+        lastImage.transform.localScale = new Vector3(float.Parse(images.Attributes.GetNamedItem("Width").Value) / lastImage.GetComponent<SpriteRenderer>().sprite.texture.width, float.Parse(images.Attributes.GetNamedItem("Height").Value) / lastImage.GetComponent<SpriteRenderer>().sprite.texture.height, 0); //Usage of Width and Height value
+
+        //Check if the image is rotated (by checking if there is a Matrix node)
+        if (images.HasChildNodes)
+            //Get into the Matrix node
+            foreach (XmlNode matrixNode in images.LastChild.FirstChild)
+            {
+                if (matrixNode.Name == "Matrix")
+                {
+                    //Set the image rotation to the A value divided by 1.665 (very wonky but kinda work for now)
+                    lastImage.transform.rotation = Quaternion.Euler(0, 0, float.Parse(matrixNode.Attributes.GetNamedItem("A").Value) / 1.665f);
+                    //Recenter the image correctly
+                    lastImage.transform.position = new Vector3
+                    (lastImage.transform.position.x + float.Parse(matrixNode.Attributes.GetNamedItem("Tx").Value) / 100,
+                    lastImage.transform.position.y + float.Parse(matrixNode.Attributes.GetNamedItem("D").Value) / 100,
+                    0);
+                }
+            }
+        DestroyImmediate(dummyObject); //Remove duplicated images
     }
 }
